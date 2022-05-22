de7d167a0ebbe9403d83857c7ba5a7275696f9c8
Pareeesa <parisa.faridi80@gmail.com>
2022-05-22 18:28:20 +0430

    Give default value to page property

--- app/src/main/java/com/example/homework17/network/ApiService.kt
+++ app/src/main/java/com/example/homework17/network/ApiService.kt
@@ -4,14 +4,13 @@ import com.example.homework17.data.model.MovieList
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val API_KEY = "b8fb74a7f7ebe3f2402f6de80059d5a5"
+private const val API_KEY = "a22313b82c44d5f5f21b763ba898a2a7"
 
 interface ApiService {
 
     @GET("movie/popular")
     suspend fun getPopular(
-        @Query("page") page:Int,
+        @Query("page")page :Int = 1,
         @Query("api_key") apiKey:String = API_KEY
     ): MovieList
-
 }
