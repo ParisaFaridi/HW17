619c56c220918e3d54fae36d8269d66097ed94c2
Pareeesa <parisa.faridi80@gmail.com>
2022-05-23 16:38:53 +0430

    Create recycler view adapter and use it to show list of movies

---
+++ app/src/main/java/com/example/homework17/MovieAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.homework17
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.homework17.data.model.Movie
+import com.example.homework17.databinding.MovieItemBinding
+import com.example.homework17.network.poster_path
+
+class MovieAdapter:
+    ListAdapter<Movie, MovieAdapter.ItemHolder>(FormulaDiffCallBack) {
+
+    object FormulaDiffCallBack: DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+    class ItemHolder(val binding: MovieItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val binding: MovieItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.movie_item,
+            parent,
+            false
+        )
+        return ItemHolder(binding)
+    }
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        holder.binding.movie = getItem(position)
+        Glide.with(holder.binding.imageView.context).load(poster_path + getItem(position).poster_path)
+            .into(holder.binding.imageView)
+    }
+}
