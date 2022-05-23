5e7e0636d0fb5c61161d74ce9071b14f0cf0b93e
Pareeesa <parisa.faridi80@gmail.com>
2022-05-23 16:41:50 +0430

    Implement viewModel class

--- app/src/main/java/com/example/homework17/ui/homefragment/HomeViewModel.kt
+++ app/src/main/java/com/example/homework17/ui/homefragment/HomeViewModel.kt
@@ -4,15 +4,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.homework17.data.datasources.RemoteDataSource
+import com.example.homework17.data.model.Movie
 import kotlinx.coroutines.launch
 
-class HomeViewModel(val remoteDataSource: RemoteDataSource) : ViewModel() {
+class HomeViewModel(private val remoteDataSource: RemoteDataSource) : ViewModel() {
 
-    val moviewO = MutableLiveData("not")
-    fun getPopularMovie(){
+    private val status = MutableLiveData<ApiStatus>()
+    val movies = MutableLiveData<List<Movie>>()
+
+    init {
+        getPopularMovie()
+    }
+
+    private fun getPopularMovie() {
         viewModelScope.launch {
-            val list = remoteDataSource.getPopular()
-            moviewO.value = list[0].overview
+            status.value = ApiStatus.LOADING
+            movies.value = remoteDataSource.getPopular()
+            status.value = ApiStatus.DONE
         }
     }
 }
+
+enum class ApiStatus { LOADING, ERROR, DONE }
