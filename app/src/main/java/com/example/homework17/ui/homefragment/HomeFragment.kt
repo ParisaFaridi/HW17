be8da63e3dcf9c02653583dd25bb774f94e7d334
Pareeesa <parisa.faridi80@gmail.com>
2022-05-23 16:51:42 +0430

    Add gridView to home fragment add fix posters size

--- app/src/main/java/com/example/homework17/ui/homefragment/HomeFragment.kt
+++ app/src/main/java/com/example/homework17/ui/homefragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.homework17.MovieAdapter
 import com.example.homework17.databinding.FragmentHomeBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -24,6 +25,7 @@ class HomeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
         val adapter = MovieAdapter()
         viewModelHome.movies.observe(viewLifecycleOwner) {
             binding.recyclerView.adapter = adapter
