701e0c45dcfd85b642b421c50f34f7c1e9acc4d5
Pareeesa <parisa.faridi80@gmail.com>
2022-05-24 13:20:50 +0430

    Navigate to detail fragment from upcoming fragment

--- app/src/main/java/com/example/homework17/ui/upcomingfragment/UpcomingFragment.kt
+++ app/src/main/java/com/example/homework17/ui/upcomingfragment/UpcomingFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.homework17.MovieAdapter
 import com.example.homework17.databinding.FragmentUpcomingBinding
+import com.example.homework17.ui.homefragment.HomeFragmentDirections
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -26,8 +28,12 @@ class UpcomingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity?.title = "Upcoming Movies"
         binding.recyclerView.layoutManager = GridLayoutManager(requireContext(),2)
-        val adapter = MovieAdapter{}
+        val adapter = MovieAdapter{
+            val action = UpcomingFragmentDirections.actionUpcomingFragmentToDetailFragment(it.id)
+            findNavController().navigate(action)
+        }
         upComingViewModel.upComingMovies.observe(viewLifecycleOwner){
             binding.recyclerView.adapter = adapter
             adapter.submitList(it)
