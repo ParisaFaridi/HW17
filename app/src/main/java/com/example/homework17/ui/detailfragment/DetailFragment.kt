77568e3840803c7d66b70ab0eb51377881bcb41c
Pareeesa <parisa.faridi80@gmail.com>
2022-05-24 13:42:51 +0430

    Add new button to DetailFragment for navigating to TrailerFragment

--- app/src/main/java/com/example/homework17/ui/detailfragment/DetailFragment.kt
+++ app/src/main/java/com/example/homework17/ui/detailfragment/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.homework17.R
@@ -38,6 +39,15 @@ class DetailFragment : Fragment() {
             Glide.with(binding.imageView2).load(POSTER_PATH + it.poster_path)
                 .into(binding.imageView2)
             activity?.title = it.title
+        }
+        binding.btnShowTrailer.setOnClickListener {
+            val action = detailViewModel.movie.value?.let { it1 ->
+                DetailFragmentDirections.actionDetailFragmentToTrailerFragment(
+                    it1.id)
+            }
+            if (action != null) {
+                findNavController().navigate(action)
+            }
         }
 
     }
