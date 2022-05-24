a337f7b99e391cbaefdcc6cf9698c68a0a39d124
Pareeesa <parisa.faridi80@gmail.com>
2022-05-24 15:45:32 +0430

    Implement webView

--- app/src/main/java/com/example/homework17/ui/trailerfragment/TrailerFragment.kt
+++ app/src/main/java/com/example/homework17/ui/trailerfragment/TrailerFragment.kt
@@ -2,18 +2,19 @@ package com.example.homework17.ui.trailerfragment
 
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
 import android.webkit.WebSettings
 import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.example.homework17.R
-import com.example.homework17.TrailerFragmentArgs
 import com.example.homework17.databinding.FragmentTrailerBinding
+import com.example.homework17.ui.detailfragment.DetailViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TrailerFragment : Fragment() {
 
     private lateinit var binding: FragmentTrailerBinding
-    private val args:TrailerFragmentArgs by navArgs()
+    private val args: TrailerFragmentArgs by navArgs()
+    private val detailViewModel: DetailViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,22 +24,26 @@ class TrailerFragment : Fragment() {
         return binding.root
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        //args.id -> get link by api
-//        binding.webView.apply {
-//            webViewClient = WebViewClient()
-//            loadUrl(args.url)
-//        }
-//        val webSettings: WebSettings = binding.webView.settings
-//        webSettings.javaScriptEnabled = true
-//
-//        binding.webView.canGoBack()
-//        binding.webView.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
-//            if (keyCode == KeyEvent.KEYCODE_BACK && event.action === MotionEvent.ACTION_UP && binding.webView.canGoBack()) {
-//                binding.webView.goBack()
-//                return@OnKeyListener true
-//            }
-//            false
-//        })
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        detailViewModel.getTrailer(args.id)
+        detailViewModel.trailer.observe(viewLifecycleOwner) {
+            binding.webView.apply {
+                webViewClient = WebViewClient()
+                loadUrl(it)
+            }
+        }
+        val webSettings: WebSettings = binding.webView.settings
+        webSettings.javaScriptEnabled = true
+
+        binding.webView.canGoBack()
+        binding.webView.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.action === MotionEvent.ACTION_UP && binding.webView.canGoBack()) {
+                binding.webView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
     }
+}
