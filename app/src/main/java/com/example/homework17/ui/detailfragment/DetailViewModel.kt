7490641cd44bcfd3c9169bd3d3b0b801b5c66982
Pareeesa <parisa.faridi80@gmail.com>
2022-05-24 15:45:20 +0430

    Write a function for getting trailer

--- app/src/main/java/com/example/homework17/ui/detailfragment/DetailViewModel.kt
+++ app/src/main/java/com/example/homework17/ui/detailfragment/DetailViewModel.kt
@@ -14,12 +14,19 @@ class DetailViewModel(private val remoteDataSource: RemoteDataSource):ViewModel(
 
     private val status = MutableLiveData<ApiStatus>()
     val movie = MutableLiveData<Detail>()
+    val trailer =MutableLiveData<String>()
 
    fun getMovie(id :Int) {
         viewModelScope.launch {
             status.value = ApiStatus.LOADING
             movie.value = remoteDataSource.getMovieDetail(id)
             status.value = ApiStatus.DONE
+        }
+    }
+
+    fun getTrailer(id:Int){
+        viewModelScope.launch {
+            trailer.value = "https://www.youtube.com/watch?v=" + remoteDataSource.getTrailer(id).key
         }
     }
 }
