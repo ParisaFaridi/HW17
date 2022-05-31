e07c26df43b98d33120badb49b3cd327eac917db
Pareeesa <parisa.faridi80@gmail.com>
2022-05-31 10:20:29 +0430

    Implement needed queries in dao and use it LocalDataSource

--- app/src/main/java/com/example/homework17/data/datasources/LocalDataSource.kt
+++ app/src/main/java/com/example/homework17/data/datasources/LocalDataSource.kt
@@ -1,4 +1,22 @@
 package com.example.homework17.data.datasources
 
-class LocalDataSource {
+import androidx.lifecycle.LiveData
+import com.example.homework17.data.model.Movie
+import com.example.homework17.room.MovieDatabase
+
+class LocalDataSource(private val movieDb: MovieDatabase) {
+
+    suspend fun insert(movies: List<Movie>){
+        for (i in movies)
+            movieDb.movieDao.insert(i)
+    }
+    suspend fun getPopular():List<Movie> {
+        return movieDb.movieDao.getPopular()
+    }
+    suspend fun getUpcoming(): List<Movie> {
+        return movieDb.movieDao.getUpcoming()
+    }
+    suspend fun getDetail(id:Int):Movie{
+        return movieDb.movieDao.getDetail(id)
+    }
 }
--- app/src/main/java/com/example/homework17/room/MovieDao.kt
+++ app/src/main/java/com/example/homework17/room/MovieDao.kt
@@ -2,18 +2,22 @@ package com.example.homework17.room
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.homework17.data.model.Movie
 
 @Dao
 interface MovieDao {
 
-    @Insert
-    suspend fun insert(vararg movie:Movie)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(movie: Movie)
 
-    @Query("")
-    suspend fun getPopular()
+    @Query("SELECT * FROM Movie WHERE isUpcoming = 0")
+    suspend fun getPopular():List<Movie>
 
-    @Query("")
-    suspend fun getUpcoming()
+    @Query("SELECT * FROM Movie WHERE isUpcoming = 1")
+    suspend fun getUpcoming():List<Movie>
+
+    @Query("SELECT * FROM MOVIE WHERE id = :id LIMIT 1")
+    suspend fun getDetail(id:Int):Movie
 }
