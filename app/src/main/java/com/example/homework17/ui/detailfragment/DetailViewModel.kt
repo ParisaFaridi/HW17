d9ceeb131134687f9d21e1250beb95218202ca03
Pareeesa <parisa.faridi80@gmail.com>
2022-06-03 14:50:04 +0430

    Create an extension function for checking internet and use it viewModel

--- app/src/main/java/com/example/homework17/ui/detailfragment/DetailViewModel.kt
+++ app/src/main/java/com/example/homework17/ui/detailfragment/DetailViewModel.kt
@@ -1,17 +1,13 @@
 package com.example.homework17.ui.detailfragment
 
 import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.homework17.App
 import com.example.homework17.Resource
 import com.example.homework17.data.Repository
 import com.example.homework17.data.model.Movie
+import com.example.homework17.hasInternetConnection
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
@@ -40,30 +36,5 @@ class DetailViewModel(private val repository: Repository,app: Application) : And
             }
         }
         return Resource.Error(response.message())
-    }
-    private fun hasInternetConnection():Boolean{
-        val connectivityManager = getApplication<App>().getSystemService(
-            Context.CONNECTIVITY_SERVICE
-        ) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            val activeNetwork = connectivityManager.activeNetwork ?: return  false
-            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?:return false
-            return when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
-        }else{
-            connectivityManager.activeNetworkInfo?.run{
-                return when(type){
-                    ConnectivityManager.TYPE_WIFI -> true
-                    ConnectivityManager.TYPE_MOBILE ->true
-                    ConnectivityManager.TYPE_ETHERNET -> true
-                    else ->false
-                }
-            }
-        }
-        return false
     }
 }
--- app/src/main/java/com/example/homework17/ui/upcomingfragment/UpComingViewModel.kt
+++ app/src/main/java/com/example/homework17/ui/upcomingfragment/UpComingViewModel.kt
@@ -1,18 +1,14 @@
 package com.example.homework17.ui.upcomingfragment
 
 import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities.*
-import android.os.Build
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.homework17.App
 import com.example.homework17.Resource
 import com.example.homework17.data.Repository
 import com.example.homework17.data.model.Movie
 import com.example.homework17.data.model.MovieList
+import com.example.homework17.hasInternetConnection
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
@@ -46,30 +42,4 @@ class UpComingViewModel (private val repository: Repository,app: Application) : 
             }
             return Resource.Error(response.message())
     }
-    private fun hasInternetConnection():Boolean{
-        val connectivityManager = getApplication<App>().getSystemService(
-            Context.CONNECTIVITY_SERVICE
-        ) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            val activeNetwork = connectivityManager.activeNetwork ?: return  false
-            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?:return false
-            return when {
-                capabilities.hasTransport(TRANSPORT_WIFI) -> true
-                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
-                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
-        }else{
-            connectivityManager.activeNetworkInfo?.run{
-                return when(type){
-                    ConnectivityManager.TYPE_WIFI -> true
-                    ConnectivityManager.TYPE_MOBILE ->true
-                    ConnectivityManager.TYPE_ETHERNET -> true
-                    else ->false
-                }
-            }
-        }
-        return false
-    }
-
 }
